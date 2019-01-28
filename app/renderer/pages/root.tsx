0572e088c4c526a72273d497f207f7e01f24cb97
Zephraph <zephraph@gmail.com>
2019-01-27 22:07:24 -0500

    Remove sidebar to simplify things

--- app/renderer/pages/root.tsx
+++ app/renderer/pages/root.tsx
@@ -1,34 +1,33 @@
 import React from "react";
-import { Layout, Steps } from "antd";
+import { Layout, Steps, Button } from "antd";
 import { RepositorySelector } from "../components/RepositorySelector";
 import { Observer } from "mobx-react";
 import { store } from "../store";
 import { RepositoryList } from "../components/RepositoryList";
+import { searchForFileInRepos } from "../lib/github";
 
-const { Content, Sider } = Layout;
+const { Content, Footer, Sider } = Layout;
 const { Step } = Steps;
 
 export default () => (
   <Layout>
-    <Sider
-      width={230}
-      style={{
-        background: "#F0F2F5",
-        padding: "20px",
-        borderRight: "1px solid #F0F2F5",
-        height: "100%"
-      }}
-    >
-      <Steps direction="vertical" size="small" current={0}>
-        <Step title="Select Repositories" />
-        <Step title="In Progress" />
-        <Step title="Waiting" />
-      </Steps>
-    </Sider>
     <Content style={{ background: "white", padding: "20px" }}>
       <h4>Select repositories</h4>
       <RepositorySelector onSelect={(repo) => store.addRepositories([repo])} />
       <RepositoryList />
     </Content>
+    <Footer style={{ background: "white", padding: "20px" }}>
+      <Button
+        type="primary"
+        onClick={() => {
+          searchForFileInRepos(
+            ["artsy/palette", "artsy/reaction"],
+            ".autorc"
+          ).then((result) => console.log(result));
+        }}
+      >
+        Next
+      </Button>
+    </Footer>
   </Layout>
 );
